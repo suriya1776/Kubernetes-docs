c84631c31b3c3266832c1596ef58e4e4cf7074cc
suriya <suriyamuthukumar1776@gmail.com>
2024-01-27 11:16:54 +0530

    added shell script

--- cluster-management/cluster-creation.sh
+++ cluster-management/cluster-creation.sh
@@ -11,7 +11,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 #export config path
 export KUBECONFIG=/etc/kubernetes/admin.conf
 #joining the node machine
-kubeadm join 192.168.0.8:6443 --token c3u6xu.evrwojz3mvcw8ulv --discovery-token-ca-cert- hash sha256:22acffd2bc6c76faa4a8d0c3ba6e22ca2556b127f86641fb552e575d0c05f582
+#kubeadm join 192.168.0.8:6443 --token c3u6xu.evrwojz3mvcw8ulv --discovery-token-ca-cert- hash sha256:22acffd2bc6c76faa4a8d0c3ba6e22ca2556b127f86641fb552e575d0c05f582
 #For initlizing the network
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+
+
 
