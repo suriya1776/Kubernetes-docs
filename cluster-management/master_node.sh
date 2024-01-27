50c4a26af16da1a52893e1871c30f339a530ac22
suriya <suriyamuthukumar1776@gmail.com>
2024-01-27 13:19:25 +0530

    modified word doc

--- cluster-management/master_node.sh
+++ cluster-management/master_node.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 #initize a api server and advertising api
-kubeadm init --apiserver-advertise-address $(hostname -i) -- pod-network-cidr 10.5.0.0/16
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
 #make dir for config
 mkdir -p $HOME/.kube
 #copy the config created and copy to your home
@@ -10,10 +10,12 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 #export config path
 export KUBECONFIG=/etc/kubernetes/admin.conf
+join_command=$(kubeadm token create --print-join-command)
 #joining the node machine
+eval "$join_command"
+
 #kubeadm join 192.168.0.8:6443 --token c3u6xu.evrwojz3mvcw8ulv --discovery-token-ca-cert- hash sha256:22acffd2bc6c76faa4a8d0c3ba6e22ca2556b127f86641fb552e575d0c05f582
 #For initlizing the network
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
-
-
+echo "Join command + $join_command"
 
