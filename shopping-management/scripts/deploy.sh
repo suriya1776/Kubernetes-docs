f3fcfd381bc524837d1354845757649f58b6bb8f
suriya <suriyamuthukumar1776@gmail.com>
2024-02-03 20:23:56 +0530

    added a deloy script

---
+++ shopping-management/scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+kubectl create -f ../configmap/order-service-config.yaml
+kubectl create -f ../configmap/payment-service-config.yaml
+kubectl create -f ../configmap/product-service-config.yaml
+
+kubectl create -f ../services/mysql-service.yaml
+kubectl create -f ../services/order-service-service.yaml
+kubectl create -f ../services/payment-service-service.yaml
+kubectl create -f ../services/product-service-service.yaml
+
+kubectl create -f ../deployment/order-service-deployment.yaml
+kubectl create -f ../deployment/payment-service-deployment.yaml
+kubectl create -f ../deployment/product-service-deployment.yaml
+kubectl create -f ../deployment/mysql-deployment.yaml
+
